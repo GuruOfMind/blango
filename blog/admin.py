13f3211cc381d797c64219df0987b75bac820d04
Almkdad Ali <almkdad.m.ali@gmail.com>
2021-11-21 11:17:04 +0000

    Finish Introduction and Django Admin- MArk all sections as completed

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,7 @@ admin.site.register(Tag)
 
 class PostAdmin(admin.ModelAdmin):
   prepopulated_fields = {"slug": ("title",)}
+  list_display = ("slug", "published_at")
 # exclude : list of fields disallowed to edit in the admin
 # fields  : opposite of exclude
 # list_display: a list of fields to include in the admin page list view

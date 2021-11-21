74e39a0ff59b51752493ab5c18e11fe7ee234b84
Almkdad Ali <almkdad.m.ali@gmail.com>
2021-11-21 11:02:45 +0000

    Finish Introduction and Django Admin

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from blog.models import Tag, Post
+
+# Register your models here.
+admin.site.register(Tag)
+
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("title",)}
+# exclude : list of fields disallowed to edit in the admin
+# fields  : opposite of exclude
+# list_display: a list of fields to include in the admin page list view
+
+admin.site.register(Post, PostAdmin)
